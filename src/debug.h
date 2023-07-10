1b29115f66f355f8b9d5cef93e531359cdc0be54
XeNoCrAtEz <joydamanik@gmail.com>
2023-07-10 20:05:25 +0700

    fixed redefine DEBUG

--- src/debug.h
+++ src/debug.h
@@ -1,7 +1,5 @@
 #include <Arduino.h>
 
-#define DEBUG
-
 void info_soil_data(
     const int& nitrogen, const int& phosphorus, const int& kalium,
     const float& pH, const float& temperature, const float& humidity,
