72f2f3ee226a64e4cc973ad5dff8217712938877
XeNoCrAtEz <joydamanik@gmail.com>
2023-10-31 14:13:19 +0700

    added pin assignment header

--- lib/display/display.h
+++ lib/display/display.h
@@ -20,8 +20,7 @@
 // Screen constants parameters
 const int SCREEN_WIDTH = 128;   // OLED display width, in pixels
 const int SCREEN_HEIGHT = 64;   // OLED display height, in pixels
-const int SCREEN_SDA = 5;
-const int SCREEN_SCL = 4;
+
 const int SCREEN_ADDR = 0x3C;
 
 void setup_display();
---
+++ src/pin_assignment.h
@@ -0,0 +1,12 @@
+#ifndef __PIN_ASSIGNMENT_H__
+#define __PIN_ASSIGNMENT_H__
+
+
+const int SCREEN_SDA = 5;
+const int SCREEN_SCL = 4;
+
+const int PROBE_RX_PIN = 16;
+const int PROBE_TX_PIN = 17;
+
+
+#endif
