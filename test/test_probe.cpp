34471f30f69ea9b13b4fb5008a1043adbcd244b6
XeNoCrAtEz <joydamanik@gmail.com>
2023-11-01 15:53:36 +0700

    added probe unit-testing

---
+++ test/test_probe.cpp
@@ -0,0 +1,50 @@
+#include <Arduino.h>
+#include <unity.h>
+
+#include "probe.h"
+#include "display.h"
+
+
+const uint8_t PIN_PROBE_RX = 16;
+const uint8_t PIN_PROBE_TX = 17;
+const uint8_t PIN_SDA = 21;
+const uint8_t PIN_SCL = 22;
+
+
+void test_ProbeKHDTK_sample() {
+    ProbeKHDTK testProbe = ProbeKHDTK(PIN_PROBE_RX, PIN_PROBE_TX);
+
+    SoilData sampledData = SoilData();
+    TEST_ASSERT_EQUAL(Probe::SUCCESS, testProbe.sample(sampledData));
+
+    Display display(PIN_SDA, PIN_SCL);
+    display.display_data(sampledData);
+}
+
+
+void test_ProbeDefault_sample() {
+    ProbeDefault testProbe = ProbeDefault(PIN_PROBE_RX, PIN_PROBE_TX);
+
+    SoilData sampledData = SoilData();
+    TEST_ASSERT_EQUAL(Probe::SUCCESS, testProbe.sample(sampledData));
+
+    Display display(PIN_SDA, PIN_SCL);
+    display.display_data(sampledData);
+}
+
+void test_ProbeNew_sample() {
+    ProbeNew testProbe = ProbeNew(PIN_PROBE_RX, PIN_PROBE_TX);
+
+    SoilData sampledData = SoilData();
+    TEST_ASSERT_EQUAL(Probe::SUCCESS, testProbe.sample(sampledData));
+
+    Display display(PIN_SDA, PIN_SCL);
+    display.display_data(sampledData);
+}
+
+
+void test_probe() {
+    RUN_TEST(test_ProbeKHDTK_sample);
+    RUN_TEST(test_ProbeDefault_sample);
+    RUN_TEST(test_ProbeNew_sample);
+}
