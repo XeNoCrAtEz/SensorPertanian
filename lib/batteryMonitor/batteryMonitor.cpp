e43757c226dac98306f3c53579a8aacc29f1c45b
XeNoCrAtEz <joydamanik@gmail.com>
2023-11-01 15:44:04 +0700

    revert to default argument

--- lib/batteryMonitor/batteryMonitor.cpp
+++ lib/batteryMonitor/batteryMonitor.cpp
@@ -1,14 +1,7 @@
 #include "batteryMonitor.h"
 
 
-const uint16_t BatteryMonitor::DEFAULT_MIN_VOLT = 3700;    // mV
-const uint16_t BatteryMonitor::DEFAULT_MAX_VOLT = 4200;    // mV
-const uint16_t BatteryMonitor::DEFAULT_VREF_VOLT = 900;    // mV
-const uint8_t  BatteryMonitor::DEFAULT_RATIO = 2;
-
-
 BatteryMonitor::BatteryMonitor(uint8_t pin, uint16_t min, uint16_t max, uint16_t vref, float ratio)
-        : Battery{min, max, pin},
-          MIN_VOLT{min}, MAX_VOLT{max}, VREF_VOLT{vref}, RATIO{ratio} {
+        : Battery{min, max, pin} {
     begin(vref, ratio);
 }
--- lib/batteryMonitor/batteryMonitor.h
+++ lib/batteryMonitor/batteryMonitor.h
@@ -6,21 +6,8 @@
 #include <Battery.h>
 
 class BatteryMonitor : public Battery {
-private:
-    const uint16_t MIN_VOLT;    // mV
-    const uint16_t MAX_VOLT;    // mV
-    const uint16_t VREF_VOLT;    // mV
-    const uint8_t  RATIO;
-
-
-    static const uint16_t DEFAULT_MIN_VOLT;    // mV
-    static const uint16_t DEFAULT_MAX_VOLT;    // mV
-    static const uint16_t DEFAULT_VREF_VOLT;    // mV
-    static const uint8_t  DEFAULT_RATIO;
-
-
 public:
-    BatteryMonitor(uint8_t pin, uint16_t min=DEFAULT_MIN_VOLT, uint16_t max=DEFAULT_MAX_VOLT, uint16_t vref=DEFAULT_VREF_VOLT, float ratio=DEFAULT_RATIO);
+    BatteryMonitor(uint8_t pin, uint16_t min=3700, uint16_t max=4200, uint16_t vref=900, float ratio=2);
 };
 
 
