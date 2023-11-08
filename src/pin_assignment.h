f915ef1bf49f24bbd688ccb3b11e23f171d33b14
XeNoCrAtEz <joydamanik@gmail.com>
2023-11-08 10:38:16 +0700

    renamed pin assignments

--- src/pin_assignment.h
+++ src/pin_assignment.h
@@ -2,11 +2,11 @@
 #define __PIN_ASSIGNMENT_H__
 
 
-const int SCREEN_SDA = 5;
-const int SCREEN_SCL = 4;
+const int PIN_SCREEN_SDA = 5;
+const int PIN_SCREEN_SCL = 4;
 
-const int PROBE_RX_PIN = 16;
-const int PROBE_TX_PIN = 17;
+const int PIN_PROBE_RX = 16;
+const int PIN_PROBE_TX = 17;
 
 
 #endif
