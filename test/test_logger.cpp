3e8f523e54908224e3926486d8250142c7c3d9ad
XeNoCrAtEz <joydamanik@gmail.com>
2024-01-11 11:57:52 +0700

    added test for clear(), show(), and fix for show() changes

--- test/test_logger.cpp
+++ test/test_logger.cpp
@@ -8,6 +8,17 @@ const int PIN_RTC_DATA = 13;
 const int PIN_RTC_CLK  = 15;
 
 
+void test_show() {
+    RTC testRTC = RTC(PIN_RTC_DATA, PIN_RTC_CLK, PIN_RTC_RST);
+
+    auto testDebug = Logger(testRTC);
+    TEST_ASSERT_EQUAL(true, testDebug.is_ready());
+
+    auto errCode = testDebug.show();
+    TEST_ASSERT_EQUAL(Logger::SUCCESS, errCode);
+}
+
+
 void test_single_msg() {
     String expectedMsg = "1 [E] - Error! JK just a normal message :)";
 
@@ -32,7 +43,7 @@ void test_single_msg_print_mode() {
     RTC testRTC = RTC(PIN_RTC_DATA, PIN_RTC_CLK, PIN_RTC_RST);
 
     auto testDebug = Logger(testRTC, true);
-    TEST_ASSERT_EQUAL(false, testDebug.is_ready());
+    TEST_ASSERT_EQUAL(true, testDebug.is_ready());
     TEST_ASSERT_EQUAL(true, testDebug.is_print_mode());
 
     Logger::ErrorCodes errCode = testDebug.log_E("Error! JK just a normal message :)");
@@ -40,10 +51,13 @@ void test_single_msg_print_mode() {
 
     errCode = testDebug.show();
     TEST_ASSERT_EQUAL(Logger::PRINT_MODE, errCode);
+
+    testDebug.clear();
 }
 
 
 // NOTE: run this without -DDEBUG compilation flag
+#ifndef DEBUG
 void test_debug_disabled() {
     String expectedMsg = "1 [E] - Error! JK just a normal message :)";
 
@@ -58,11 +72,29 @@ void test_debug_disabled() {
 
     errCode = testDebug.show();
     TEST_ASSERT_EQUAL(Logger::DEBUG_INACTIVE, errCode);
+
+    testDebug.clear();
+}
+#endif
+
+
+void test_clear() {
+    RTC testRTC = RTC(PIN_RTC_DATA, PIN_RTC_CLK, PIN_RTC_RST);
+
+    auto testDebug = Logger(testRTC);
+    TEST_ASSERT_EQUAL(true, testDebug.is_ready());
+
+    auto errCode = testDebug.clear();
+    TEST_ASSERT_EQUAL(Logger::SUCCESS, errCode);
 }
 
 
 void test_logger() {
-    RUN_TEST(test_single_msg);
-    RUN_TEST(test_single_msg_print_mode);
-    RUN_TEST(test_debug_disabled);
+    RUN_TEST(test_show);
+    // RUN_TEST(test_single_msg);
+    // RUN_TEST(test_single_msg_print_mode);
+#ifndef DEBUG
+    // RUN_TEST(test_debug_disabled);
+#endif
+    // RUN_TEST(test_clear);
 }
