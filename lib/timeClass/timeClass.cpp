f36ce12336af6c3bef5d3e0690861fa1129c67d8
XeNoCrAtEz <joydamanik@gmail.com>
2024-01-11 15:53:56 +0700

    added timeClass

---
+++ lib/timeClass/timeClass.cpp
@@ -0,0 +1,22 @@
+#include "timeClass.h"
+
+
+TimeClass::TimeClass(RTC& rtc, Submitter& submitter) 
+        : rtc(rtc), submitter(submitter) {
+    RTCAvailable = rtc.is_ready();
+    NTPAvailable = submitter.is_time_available();
+    
+    if (NTPAvailable && RTCAvailable) update_RTC();
+}
+
+
+TimeClass::ErrorCodes TimeClass::update_RTC() {
+    rtc.set_date_time(submitter.get_current_time());
+}
+
+
+RtcDateTime TimeClass::get_time() {
+    if (RTCAvailable) return rtc.get_date_time();
+    else if (NTPAvailable) return submitter.get_current_time();
+    else return RtcDateTime();
+}
