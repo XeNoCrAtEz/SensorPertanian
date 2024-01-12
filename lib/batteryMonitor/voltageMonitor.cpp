9603bbc5c504e8ea90811209fd3b946998cea45b
XeNoCrAtEz <joydamanik@gmail.com>
2024-01-12 16:41:13 +0700

    putting const on everything as it should be

--- lib/batteryMonitor/voltageMonitor.cpp
+++ lib/batteryMonitor/voltageMonitor.cpp
@@ -2,14 +2,12 @@
 
 
 VoltageMonitor::VoltageMonitor(uint8_t sensePin, uint16_t refVoltage, float dividerRatio) 
-        : m_sensePin(sensePin) {
-    m_refVoltage = refVoltage;
-	m_dividerRatio = dividerRatio;
+		: m_sensePin(sensePin), m_refVoltage(refVoltage), m_dividerRatio(dividerRatio) {
 	pinMode(m_sensePin, INPUT);
 }
 
 
-uint16_t VoltageMonitor::voltage() {
+uint16_t VoltageMonitor::voltage() const {
 	analogRead(m_sensePin);   delay(2);   // allow the ADC to stabilize
 	uint16_t reading = analogRead(m_sensePin) * m_dividerRatio * m_refVoltage / ADC_MAX_VALUE;
 	return reading;
@@ -22,14 +20,14 @@ BatteryMonitor::BatteryMonitor(
 	uint16_t minVoltage, uint16_t maxVoltage) 
         : VoltageMonitor(sensePin, refVoltage, dividerRatio),
 		  m_minVoltage(minVoltage), m_maxVoltage(maxVoltage) {}
-    
-    
-uint8_t BatteryMonitor::level() {
+
+
+uint8_t BatteryMonitor::level() const {
     return level(voltage());
 }
 
 
-uint8_t BatteryMonitor::level(uint16_t voltage) {
+uint8_t BatteryMonitor::level(uint16_t voltage) const {
     if (voltage <= m_minVoltage) {
 		return 0;
 	} else if (voltage >= m_maxVoltage) {
--- lib/batteryMonitor/voltageMonitor.h
+++ lib/batteryMonitor/voltageMonitor.h
@@ -20,13 +20,13 @@ private:
 
 public:
     VoltageMonitor(uint8_t sensePin, uint16_t refVoltage, float dividerRatio);
-    uint16_t voltage();
+    uint16_t voltage() const;
 
 
 protected:
-    uint16_t m_refVoltage;        // refVoltage is in mV, typically the value of max ADC voltage
-    float m_dividerRatio;         // value of (R1 + R2) / R2 in a typical voltage divider circuit
-    uint8_t m_sensePin;
+    const uint16_t m_refVoltage;        // refVoltage is in mV, typically the value of max ADC voltage
+    const float m_dividerRatio;         // value of (R1 + R2) / R2 in a typical voltage divider circuit
+    const uint8_t m_sensePin;
 
 
 };
@@ -37,13 +37,13 @@ public:
     BatteryMonitor(
         uint8_t sensePin, uint16_t refVoltage, float dividerRatio, 
         uint16_t minVoltage, uint16_t maxVoltage);
-    uint8_t level();
-    uint8_t level(uint16_t voltage);
+    uint8_t level() const;
+    uint8_t level(uint16_t voltage) const;
 
 
 private:
-    uint16_t m_minVoltage;        // value of measured voltage (in mV) that corresponds to 0%
-    uint16_t m_maxVoltage;        // value of measured voltage (in mV) that corresponds to 100%
+    const uint16_t m_minVoltage;        // value of measured voltage (in mV) that corresponds to 0%
+    const uint16_t m_maxVoltage;        // value of measured voltage (in mV) that corresponds to 100%
 
 };
 
