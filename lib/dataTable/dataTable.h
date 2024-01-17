7bf63ff9ad9ff5226f52c17371eaa56946186f00
XeNoCrAtEz <joydamanik@gmail.com>
2024-01-17 10:20:32 +0700

    move SoilReading to soilData.h

--- lib/dataTable/dataTable.h
+++ lib/dataTable/dataTable.h
@@ -7,25 +7,6 @@
 #include "soil_data.h"
 
 
-
-class SoilReading {
-public:
-    uint32_t m_epoch;         // seconds from 2000
-    SoilData m_soilData;
-
-
-    SoilReading()
-            : m_soilData{}, m_epoch{0} {}
-    SoilReading(const SoilData& data, const uint32_t& epch)
-            : m_soilData{data}, m_epoch{epch} { }
-
-    bool operator==(const SoilReading& s2) {
-        return m_soilData == s2.m_soilData &&
-                m_epoch == s2.m_epoch;
-    }
-};
-
-
 class SoilDataTable {
 public:
     // class status codes
