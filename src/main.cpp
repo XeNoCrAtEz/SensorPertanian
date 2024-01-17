7c1564acad94edb74ff27bb59f826f645e07fcbc
XeNoCrAtEz <joydamanik@gmail.com>
2024-01-17 10:00:28 +0700

    change into global variables

--- src/main.cpp
+++ src/main.cpp
@@ -1,92 +1,112 @@
 #include "main.h"
 
+
+RTC rtc = RTC(PIN_RTC_DATA, PIN_RTC_CLK, PIN_RTC_RST);
+
+#if defined(USE_WIFI)
+SubmitterWiFi submitter;
+#elif defined(USE_GSM)
+SubmitterGSM submitter(PIN_GSM_RX, PIN_GSM_TX);
+#endif
+
+TimeClass timeClass = TimeClass(rtc, submitter);
+
+Logger logger = Logger(timeClass);
+
+BatteryMonitor battMon(PIN_VOLT_BAT, ESP32_REF_VOLTAGE, VOLT_MON_DIVIDER_RATIO, MIN_VOLT_LIPO, MAX_VOLT_LIPO);
+VoltageMonitor solarCellMon(PIN_VOLT_SC, ESP32_REF_VOLTAGE, VOLT_MON_DIVIDER_RATIO);
+
+Display display(PIN_SCREEN_SDA, PIN_SCREEN_SCL);
+
+SoilDataTable dataTable;
+
+// MOSFET
+
+
 void setup() {
     // begin USB Serial
     Serial.begin(115200);
 
-log_i("Starting Sensor-%d", SENSOR_ID);
-log_i("Logging will not start until RTC, Submitter, and TimeClass are initialized.");
-    RTC rtc = RTC(PIN_RTC_DATA, PIN_RTC_CLK, PIN_RTC_RST);
+    if (logger.status() != Logger::READY) Serial.println("Error! Logger not ready! Status: " + String(logger.status()));
+    else logger.log_I("Logger is ready!");
+
+    logger.log_I("Starting Sensor-" + String(SENSOR_ID));
+
+    if (rtc.status() != RTC::READY) logger.log_E("Error! RTC not ready! Status: " + String(rtc.status()));
+    else logger.log_I("RTC ready!");
 
 #if defined(USE_WIFI)
-    SubmitterWiFi submitter;
-log_i("Using WiFi.");
+    logger.log_I("Using WiFi.");
 #elif defined(USE_GSM)
-    SubmitterGSM submitter(PIN_GSM_RX, PIN_GSM_TX);
-log_i("Using GSM.");
+    logger.log_I("Using GSM.");
 #endif
+    if (submitter.status() != Submitter::READY) logger.log_E("Error! Submitter not ready! Status: " + String(submitter.status()));
+    else logger.log_I("Submitter ready!");
 
-    TimeClass timeClass = TimeClass(rtc, submitter);
-    Logger logger = Logger(timeClass);
+    if      (timeClass.status() == TimeClass::READY) logger.log_I("NTP and RTC ready.");
+    else if (timeClass.status() == TimeClass::READY_NO_RTC) logger.log_E("Error! RTC not available!");
+    else if (timeClass.status() == TimeClass::READY_NO_NTP) logger.log_E("Error! Network time not available!");
+    else if (timeClass.status() == TimeClass::NO_TIME) logger.log_E("Error! No time available!");
 
-logger.log_I("Starting Sensor-" + String(SENSOR_ID));
-if (!timeClass.status() == TimeClass::READY_NO_RTC) logger.log_E("Error! RTC not available!");
-if (!timeClass.status() == TimeClass::READY_NO_NTP) logger.log_E("Error! Network time not available!");
-if (!timeClass.status() == TimeClass::NO_TIME) logger.log_E("Error! No time available!");
-
-    BatteryMonitor battMon(PIN_VOLT_BAT, ESP32_REF_VOLTAGE, VOLT_MON_DIVIDER_RATIO, MIN_VOLT_LIPO, MAX_VOLT_LIPO);
-    VoltageMonitor solarCellMon(PIN_VOLT_SC, ESP32_REF_VOLTAGE, VOLT_MON_DIVIDER_RATIO);
-logger.log_I("Battery voltage: " + String(battMon.voltage()) + " mV (" + String(battMon.level()) + "%)");
-logger.log_I("Solar cell voltage: " + String(solarCellMon.voltage()) + " mV");
+    logger.log_I("Battery voltage: " + String(battMon.voltage()) + " mV (" + String(battMon.level()) + "%)");
+    logger.log_I("Solar cell voltage: " + String(solarCellMon.voltage()) + " mV");
     if (battMon.level() == 0) {
-logger.log_E("Error! Battery depleted! Sleeping...");
+        logger.log_E("Error! Battery depleted! Sleeping...");
         sleep(timeClass);
     }
 
+    if (display.status() != Display::READY) logger.log_E("Error! Display is not ready! Status: " + String(display.status()));
+    else logger.log_I("Display is ready!");
+
+    if (dataTable.status() != SoilDataTable::READY) logger.log_E("Error! dataTable not ready! Status: " + String(dataTable.status()));
+    else logger.log_I("dataTable is initialized and ready!");
+
 #if defined(PROBE_DEFAULT)
     ProbeDefault probe(PIN_PROBE_RX, PIN_PROBE_TX);
-logger.log_I("Initialized probe type: DEFAULT");
+    logger.log_I("Initialized probe type: DEFAULT");
 #elif defined(PROBE_KHDTK)
     ProbeKHDTK probe(PIN_PROBE_RX, PIN_PROBE_TX);
-logger.log_I("Initialized probe type: KHDTK");
+    logger.log_I("Initialized probe type: KHDTK");
 #elif defined(PROBE_NEW)
     ProbeNew probe(PIN_PROBE_RX, PIN_PROBE_TX);
-logger.log_I("Initialized probe type: NEW");
+    logger.log_I("Initialized probe type: NEW");
 #endif
+    if (probe.status() != Probe::READY) logger.log_E("Error! Probe not ready! Status: " + String(probe.status()));
+    else logger.log_I("Probe ready!");
 
     SoilData soilData;
     Probe::OpStatus probeErr = probe.sample(soilData);
-if (probeErr == Probe::STATUS_ERROR) logger.log_E("Error! Internal probe error!");
-else if (probeErr == Probe::STATUS_NO_PROBE) logger.log_E("Error! No probe connected!");
-else logger.log_I("Sampled soil data:\n" + soilData.toString());
+    logger.log_I("Sampled soil data:\n" + soilData.toString());
 
     RtcDateTime now;
     timeClass.get_date_time(now);
     SoilReading currentReading = SoilReading(soilData, now.TotalSeconds());
 
-    SoilDataTable dataTable;
-if (!dataTable.status()) logger.log_E("Error! dataTable not ready!");
-else logger.log_I("dataTable is initialized and ready!");
-
     if (dataTable.is_empty()) {
-logger.log_I("Table is empty. Sending current sample");
+        logger.log_I("Table is empty. Sending current sample");
         int responseCode = submitter.submit_reading(currentReading);
-if (responseCode == HTTP_CODE_OK) logger.log_I("Data send successful!\n");
-else logger.log_E("Error! Data send failed! Error code: " + String(responseCode));
+        if (responseCode == HTTP_CODE_OK) logger.log_I("Data send successful!\n");
+        else logger.log_E("Error! Data send failed! Error code: " + String(responseCode));
     } else {
-logger.log_I("Saved data available. Pushing sampled data...");
+        logger.log_I("Saved data available. Pushing sampled data...");
         auto pushErr = dataTable.push(currentReading);
-if (pushErr != SoilDataTable::SUCCESS) logger.log_E("Error! Data push unsuccessful! Error code: " + String(pushErr));
+        if (pushErr != SoilDataTable::SUCCESS) logger.log_E("Error! Data push unsuccessful! Error code: " + String(pushErr));
 
         int responseCode = submitter.submit_reading(dataTable);
-if (responseCode == HTTP_CODE_OK) logger.log_I("Data send successful!\n");
-else logger.log_E("Error! Data send failed! Error code: " + String(responseCode));
-}
-
-    Display display(PIN_SCREEN_SDA, PIN_SCREEN_SCL);
-if (display.status() != Display::READY) logger.log_E("Error! Display is not ready!");
-else logger.log_I("Display is ready!");
+        if (responseCode == HTTP_CODE_OK) logger.log_I("Data send successful!\n");
+        else logger.log_E("Error! Data send failed! Error code: " + String(responseCode));
+    }
 
     display.display_splash_screen();
-logger.log_I("Splash screen displayed.");
+    logger.log_I("Splash screen displayed.");
 
     display.display_data(soilData);
-logger.log_I("Sampled soil data displayed.");
+    logger.log_I("Sampled soil data displayed.");
 
     display.clear_display();
-logger.log_I("Display cleared.");
+    logger.log_I("Display cleared.");
 
-logger.log_I("Sleeping...");
+    logger.log_I("Sleeping...");
     sleep(timeClass);
 }
 
